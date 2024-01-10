6bada8f5f81d562038de7080d2a770fdc3f22df4
pozniakoo <64778592+pozniakoo@users.noreply.github.com>
2024-01-10 12:31:38 +0100

    correct

---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "public_subnet_cidrs" {
+  type        = string
+  description = "Public subnet CIDRs"
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidrs" {
+  type        = string
+  description = "Private Subnet CIDRs"
+  default     = "10.0.2.0/24"
+}
+
+variable "azs" {
+  type        = string
+  description = "Availability Zone"
+  default     = "us-east-1a"
+}
+
+variable "ami" {
+  type    = string
+  default = "ami-0230bd60aa48260c6"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "access_key" {
+  type    = string
+  default = ""
+}
+
+variable "secret_key" {
+  type    = string
+  default = ""
+}
